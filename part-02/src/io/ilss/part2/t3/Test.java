dcef6d2505d9c80f2cbb7f8dd306ebe038448159
imyiren <imyiren4@gmail.com>
2019-01-13 00:24:19 +0800

    t3

--- part-02/src/io/ilss/part2/t3/Test.java
+++ part-02/src/io/ilss/part2/t3/Test.java
@@ -12,7 +12,8 @@ public class Test {
     public static void main(String[] args) {
         try {
             /**
-             * 如果方法B不加synchronized关键字，那么调用B方法就可能会读到脏数据，如果加了，那么在调用B方法之前已经进入A方法执行没有结束，那么B方法的调用就会出现等待。A方法结束过后才会执行B方法
+             * 如果方法B不加synchronized关键字，那么调用B方法就可能会读到脏数据;
+             * 如果加了，那么在调用B方法之前已经进入A方法执行没有结束，那么B方法的调用就会出现等待。A方法结束过后才会执行B方法
              */
             PublicVar publicVarRef = new PublicVar();
             ThreadA aThread = new ThreadA(publicVarRef);
