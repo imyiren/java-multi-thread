520ac8f1502e362395ef442031a7d7350e5640c2
ilssio <git@ilss.io>
2019-01-13 00:51:49 +0800

    synchronized lock in

---
+++ part-02/src/io/ilss/part2/synlockin_2/Main.java
@@ -0,0 +1,23 @@
+package io.ilss.part2.synlockin_2;
+
+/**
+ * className Main
+ * description
+ *
+ * @author feng
+ * @version 1.0
+ * @date 2019/1/13 上午12:44
+ */
+public class Main {
+    public int i = 10;
+
+    synchronized public void operateIMainMethod() {
+        try {
+            i--;
+            System.out.println("main print i = " + i);
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
