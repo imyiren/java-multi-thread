c2d4faded0f021db67deb52099491c1e38a5c439
imyiren <imyiren4@gmail.com>
2019-01-13 00:51:49 +0800

    synchronized lock in

---
+++ part-02/src/io/ilss/part2/synlockin_1/Service.java
@@ -0,0 +1,25 @@
+package io.ilss.part2.synlockin_1;
+
+/**
+ * className Service
+ * description
+ *
+ * @author feng
+ * @version 1.0
+ * @date 2019/1/13 上午12:39
+ */
+public class Service {
+    synchronized public void service1() {
+        System.out.println("service 1");
+        service2();
+    }
+
+    synchronized public void service2() {
+        System.out.println("service 2");
+        service3();
+    }
+
+    synchronized public void service3() {
+        System.out.println("service 3");
+    }
+}
