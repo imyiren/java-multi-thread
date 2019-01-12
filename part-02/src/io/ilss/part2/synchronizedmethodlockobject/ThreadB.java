9dcb69d8175d9615d7180ce9ffa5d70c509d3ffa
imyiren <imyiren4@gmail.com>
2019-01-13 00:01:50 +0800

    synchronized method lock object

---
+++ part-02/src/io/ilss/part2/synchronizedmethodlockobject/ThreadB.java
@@ -0,0 +1,22 @@
+package io.ilss.part2.synchronizedmethodlockobject;
+
+/**
+ * className ThreadB
+ * description
+ *
+ * @author feng
+ * @version 1.0
+ * @date 2019/1/12 下午11:55
+ */
+public class ThreadB extends Thread {
+    private MyObject object;
+
+    public ThreadB(MyObject object) {
+        this.object = object;
+    }
+    @Override
+    public void run() {
+        super.run();
+        object.methodA();
+    }
+}
