ff51a5d3b57277a388760f66d4713f1640cba293
imyiren <imyiren4@gmail.com>
2019-01-15 13:54:16 +0800

    syn block strin zero and two

---
+++ part-02/src/io/ilss/part2/synblockstring2/Run.java
@@ -0,0 +1,24 @@
+package io.ilss.part2.synblockstring2;
+
+/**
+ * className Run
+ * description
+ *
+ * @author feng
+ * @version 1.0
+ * @date 2019/1/15 下午1:48
+ */
+public class Run {
+    public static void main(String[] args) {
+        /**
+         * 验证synchronized方法与同步synchronized(非this对象)是异步调用
+         */
+        Service service = new Service();
+        ThreadA aThread = new ThreadA(service);
+        ThreadB bThread = new ThreadB(service);
+        aThread.setName("A");
+        bThread.setName("B");
+        aThread.start();
+        bThread.start();
+    }
+}
