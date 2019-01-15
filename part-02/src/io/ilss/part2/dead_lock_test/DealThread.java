315bfe609ddfa1951b0a0176144fde6be2f94c3b
ilssio <git@ilss.io>
2019-01-15 18:23:48 +0800

    dead lock test 死锁

---
+++ part-02/src/io/ilss/part2/dead_lock_test/DealThread.java
@@ -0,0 +1,51 @@
+package io.ilss.part2.dead_lock_test;
+
+/**
+ * className DealThread
+ * description
+ *
+ * @author feng
+ * @version 1.0
+ * @date 2019/1/15 下午5:50
+ */
+public class DealThread implements Runnable {
+    public String username;
+    public Object lock1 = new Object();
+    public Object lock2 = new Object();
+
+    public void setFlag(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public void run() {
+        if ("a".equals(username)) {
+            synchronized (lock1) {
+                try {
+                    System.out.println("username = " + username);
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                synchronized (lock2) {
+                    System.out.println("lock1 -> lock2");
+                }
+            }
+        }
+        if ("b".equals(username)) {
+            synchronized (lock2) {
+                try {
+                    System.out.println("username = " + username);
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (lock1) {
+                    System.out.println("lock2 -> lock1");
+                }
+            }
+        }
+
+    }
+}
