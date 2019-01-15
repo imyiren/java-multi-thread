c3d43f86ef8540123b0bfd0ee2a111fb85ac16a0
imyiren <imyiren4@gmail.com>
2019-01-15 16:06:02 +0800

    t9

--- part-02/src/io/ilss/part2/t9/MyService.java
+++ part-02/src/io/ilss/part2/t9/MyService.java
@@ -11,9 +11,11 @@ package io.ilss.part2.t9;
 public class MyService {
     public MyOneList addServiceMethod(MyOneList list, String data) {
         try {
-            if (list.getSize() < 1) {
-                Thread.sleep(2000);
-                list.add(data);
+            synchronized (list) {
+                if (list.getSize() < 1) {
+                    Thread.sleep(2000);
+                    list.add(data);
+                }
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
