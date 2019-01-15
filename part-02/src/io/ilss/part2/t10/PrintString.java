35f612cfdde7d0b176833dd5f1813c5f7e5efc69
ilssio <git@ilss.io>
2019-01-15 20:37:23 +0800

    t99 and t10

---
+++ part-02/src/io/ilss/part2/t10/PrintString.java
@@ -0,0 +1,36 @@
+package io.ilss.part2.t10;
+
+/**
+ * className PrintString
+ * description
+ *
+ * @author feng
+ * @version 1.0
+ * @date 2019/1/15 下午8:29
+ */
+public class PrintString implements Runnable {
+    private boolean isContinuePrint = true;
+
+    public boolean isContinuePrint() {
+        return isContinuePrint;
+    }
+
+    public void setContinuePrint(boolean isContinuePrint) {
+        this.isContinuePrint = isContinuePrint;
+    }
+
+    public void printStringMethod() {
+        try {
+            while (isContinuePrint == true) {
+                System.out.println("run printStringMethod thread name = " + Thread.currentThread().getName());
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public void run() {
+        printStringMethod();
+    }
+}
