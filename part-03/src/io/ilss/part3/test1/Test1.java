341ef045dbf957443794d82f34db2d124b94fa60
ilssio <git@ilss.io>
2019-01-15 22:20:13 +0800

    part3

---
+++ part-03/src/io/ilss/part3/test1/Test1.java
@@ -0,0 +1,15 @@
+package io.ilss.part3.test1;
+
+/**
+ * className Test1
+ * description
+ *
+ * @author feng
+ * @version 1.0
+ * @date 2019/1/15 下午10:18
+ */
+public class Test1 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ part-03/src/io/ilss/part3/two_thread_trans_data/MyList.java
@@ -0,0 +1,12 @@
+package io.ilss.part3.two_thread_trans_data;
+
+/**
+ * className MyList
+ * description
+ *
+ * @author feng
+ * @version 1.0
+ * @date 2019/1/15 下午10:17
+ */
+public class MyList {
+}
