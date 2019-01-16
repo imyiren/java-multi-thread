2f18ec1833fa83e7405b173b0a54409407690652
imyiren <imyiren4@gmail.com>
2019-01-16 18:44:11 +0800

    wait interrupt exception

--- part-03/src/io/ilss/part3/wait_interrupt_exception/Test.java
+++ part-03/src/io/ilss/part3/wait_interrupt_exception/Test.java
@@ -11,12 +11,14 @@ package io.ilss.part3.wait_interrupt_exception;
 public class Test {
     public static void main(String[] args) {
         try {
+            /**
+             * 当一个wait的线程调用interrupt方法会出现InterruptedException
+             */
             Object lock = new Object();
             ThreadA aThread = new ThreadA(lock);
             aThread.start();
             Thread.sleep(5000);
             aThread.interrupt();
-
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
