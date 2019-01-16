02d9e1af4a7b8115796a260ea2e4d9367d22e2b2
imyiren <imyiren4@gmail.com>
2019-01-17 00:12:34 +0800

    p_r_test and p_c_allWait

---
+++ part-03/src/io/ilss/part3/p_c_all_wait/C.java
@@ -0,0 +1,35 @@
+package io.ilss.part3.p_c_all_wait;
+
+/**
+ * className C
+ * description
+ *
+ * @author feng
+ * @version 1.0
+ * @date 2019/1/16 下午10:22
+ */
+public class C {
+    private String lock;
+
+    public C(String lock) {
+        this.lock = lock;
+    }
+
+    public void getValue() {
+        try {
+            synchronized (lock) {
+                Thread.sleep(100);
+                while ("".equals(ValueObject.value)) {
+                    System.out.println("consumer " + Thread.currentThread().getName() + " waiting ");
+                    lock.wait();
+                }
+                System.out.println("consumer " + Thread.currentThread().getName() + " runnable ");
+                System.out.println("get value = " + ValueObject.value);
+                ValueObject.value = "";
+                lock.notifyAll();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ part-03/src/io/ilss/part3/p_r_test/ThreadP.java
@@ -0,0 +1,25 @@
+package io.ilss.part3.p_r_test;
+
+/**
+ * className ThreadP
+ * description
+ *
+ * @author feng
+ * @version 1.0
+ * @date 2019/1/16 下午10:12
+ */
+public class ThreadP extends Thread {
+    private P p;
+
+    public ThreadP(P p) {
+        this.p = p;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        while (true) {
+            p.setValue();
+        }
+    }
+}
