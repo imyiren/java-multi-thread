f493827cfe318c33384e89c0cb0a496d255b0fa9
imyiren <imyiren4@gmail.com>
2019-01-16 17:46:15 +0800

    test1 test2 wait notify size5 3.1.3

---
+++ part-03/src/io/ilss/part3/test2/MyThread1.java
@@ -0,0 +1,31 @@
+package io.ilss.part3.test2;
+
+/**
+ * className MyThread1
+ * description
+ *
+ * @author feng
+ * @version 1.0
+ * @date 2019/1/16 下午12:05
+ */
+public class MyThread1 extends Thread {
+    private Object lock;
+
+    public MyThread1(Object lock) {
+        this.lock = lock;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        try {
+            synchronized (lock) {
+                System.out.println("begin time = " + System.currentTimeMillis());
+                lock.wait();
+                System.out.println("  end time = " + System.currentTimeMillis());
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ part-03/src/io/ilss/part3/wait_notify_size5/ThreadA.java
@@ -0,0 +1,33 @@
+package io.ilss.part3.wait_notify_size5;
+
+/**
+ * className ThreadA
+ * description
+ *
+ * @author feng
+ * @version 1.0
+ * @date 2019/1/16 下午5:28
+ */
+public class ThreadA extends Thread {
+    private Object lock;
+
+    public ThreadA(Object lock) {
+        this.lock = lock;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        try {
+            synchronized (lock) {
+                if (MyList.size() != 5) {
+                    System.out.println("wait begin " + System.currentTimeMillis());
+                    lock.wait();
+                    System.out.println("wait end " + System.currentTimeMillis());
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
