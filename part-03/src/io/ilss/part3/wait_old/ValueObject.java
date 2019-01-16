6cf2534f40265eecb8af7a840457094d3df6a068
ilssio <git@ilss.io>
2019-01-16 21:43:31 +0800

    wait old , 3.1.10

---
+++ part-03/src/io/ilss/part3/wait_old/ValueObject.java
@@ -0,0 +1,16 @@
+package io.ilss.part3.wait_old;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * className ValueObject
+ * description
+ *
+ * @author feng
+ * @version 1.0
+ * @date 2019/1/16 下午9:19
+ */
+public class ValueObject {
+    public static List<String> list = new ArrayList<>();
+}
