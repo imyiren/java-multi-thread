08aec64fe243ce8eef015650ecf3b488b2d2d12b
ilssio <git@ilss.io>
2019-01-16 11:48:52 +0800

    two thread trans data

---
+++ part-03/src/io/ilss/part3/two_thread_trans_data/ThreadB.java
@@ -0,0 +1,33 @@
+package io.ilss.part3.two_thread_trans_data;
+
+/**
+ * className ThreadB
+ * description
+ *
+ * @author feng
+ * @version 1.0
+ * @date 2019/1/16 上午11:15
+ */
+public class ThreadB extends Thread {
+    private MyList list;
+
+    public ThreadB(MyList list) {
+        this.list = list;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        try {
+            while (true) {
+                System.out.println(list.size());
+                if (list.size() == 5) {
+                    System.out.println("size == 5 , Thread B out");
+                    throw new InterruptedException();
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
