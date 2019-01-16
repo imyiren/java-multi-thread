a5e5cb9bc47e589f436e2c7e73634933ce7e9199
ilssio <git@ilss.io>
2019-01-16 17:46:15 +0800

    test1 test2 wait notify size5 3.1.3

---
+++ part-03/src/io/ilss/part3/test2/MyThread2.java
@@ -0,0 +1,27 @@
+package io.ilss.part3.test2;
+
+/**
+ * className MyThread2
+ * description
+ *
+ * @author feng
+ * @version 1.0
+ * @date 2019/1/16 下午12:07
+ */
+public class MyThread2 extends Thread {
+    private Object lock;
+
+    public MyThread2(Object lock) {
+        this.lock = lock;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        synchronized (lock) {
+            System.out.println("begin notify time " + System.currentTimeMillis());
+            lock.notify();
+            System.out.println("  end notify time " + System.currentTimeMillis());
+        }
+    }
+}
