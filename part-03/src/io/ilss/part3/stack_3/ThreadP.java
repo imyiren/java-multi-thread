9455f8412414caf2f1bcfab2537968c74cae4a51
ilssio <git@ilss.io>
2019-01-17 18:47:50 +0800

    stack

---
+++ part-03/src/io/ilss/part3/stack_3/ThreadP.java
@@ -0,0 +1,25 @@
+package io.ilss.part3.stack_3;
+
+/**
+ * className ThreadP
+ * description
+ *
+ * @author feng
+ * @version 1.0
+ * @date 2019/1/17 下午6:20
+ */
+public class ThreadP extends Thread {
+    private P p;
+
+    public ThreadP(P p) {
+        this.p = p;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        while (true) {
+            p.pushService();
+        }
+    }
+}
