5f9f79d6783dc5d7fc30864e569cd21f2dd36902
imyiren <imyiren4@gmail.com>
2019-01-17 18:32:23 +0800

    stack

---
+++ part-03/src/io/ilss/part3/stack_1/C.java
@@ -0,0 +1,21 @@
+package io.ilss.part3.stack_1;
+
+/**
+ * className C
+ * description
+ *
+ * @author feng
+ * @version 1.0
+ * @date 2019/1/17 下午6:19
+ */
+public class C {
+    private MyStack myStack;
+
+    public C(MyStack myStack) {
+        this.myStack = myStack;
+    }
+
+    public void popService() {
+        System.out.println("pop = " + myStack.pop());
+    }
+}
