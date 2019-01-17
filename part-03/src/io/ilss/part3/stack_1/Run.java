225b207f5f1191bb052d84bd835b105a41472e05
ilssio <git@ilss.io>
2019-01-17 18:32:23 +0800

    stack

---
+++ part-03/src/io/ilss/part3/stack_1/Run.java
@@ -0,0 +1,21 @@
+package io.ilss.part3.stack_1;
+
+/**
+ * className Run
+ * description
+ *
+ * @author feng
+ * @version 1.0
+ * @date 2019/1/17 下午6:22
+ */
+public class Run {
+    public static void main(String[] args) {
+        MyStack myStack = new MyStack();
+        P p = new P(myStack);
+        C c = new C(myStack);
+        ThreadP pThread = new ThreadP(p);
+        ThreadC cThread = new ThreadC(c);
+        pThread.start();
+        cThread.start();
+    }
+}
