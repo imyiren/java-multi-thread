4211a010a2d9a9ba1a96c73a469aba2d9f91a9eb
imyiren <imyiren4@gmail.com>
2019-01-17 21:01:08 +0800

    join and sleep

---
+++ part-03/src/io/ilss/part3/join_exception/ThreadA.java
@@ -0,0 +1,20 @@
+package io.ilss.part3.join_exception;
+
+/**
+ * className ThreadA
+ * description
+ *
+ * @author feng
+ * @version 1.0
+ * @date 2019/1/17 下午8:33
+ */
+public class ThreadA extends Thread {
+    @Override
+    public void run() {
+        super.run();
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            String newString = new String();
+            Math.random();
+        }
+    }
+}
---
+++ part-03/src/io/ilss/part3/join_exception/ThreadC.java
@@ -0,0 +1,23 @@
+package io.ilss.part3.join_exception;
+
+/**
+ * className ThreadC
+ * description
+ *
+ * @author feng
+ * @version 1.0
+ * @date 2019/1/17 下午8:35
+ */
+public class ThreadC extends Thread {
+    private ThreadB bThread;
+
+    public ThreadC(ThreadB bThread) {
+        this.bThread = bThread;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        bThread.interrupt();
+    }
+}
