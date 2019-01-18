a090aedd5198f335e6e4c6d90b9f3c8da8161a8c
imyiren <imyiren4@gmail.com>
2019-01-18 15:34:33 +0800

    use condition wait notify error

---
+++ part-04/src/io/ilss/use_condition_wait_notify_error/ThreadA.java
@@ -0,0 +1,23 @@
+package io.ilss.use_condition_wait_notify_error;
+
+/**
+ * className ThreadA
+ * description
+ *
+ * @author feng
+ * @version 1.0
+ * @date 2019/1/18 下午3:27
+ */
+public class ThreadA extends Thread {
+    private MyService service;
+
+    public ThreadA(MyService service) {
+        this.service = service;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        service.await();
+    }
+}
