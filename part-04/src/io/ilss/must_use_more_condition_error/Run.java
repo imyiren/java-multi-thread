d986b1d665dc81ec48776735ec2ca1df2924617f
ilssio <git@ilss.io>
2019-01-18 16:47:48 +0800

    must use more condition error

---
+++ part-04/src/io/ilss/must_use_more_condition_error/Run.java
@@ -0,0 +1,26 @@
+package io.ilss.must_use_more_condition_error;
+
+/**
+ * className Run
+ * description
+ *
+ * @author feng
+ * @version 1.0
+ * @date 2019/1/18 下午4:45
+ */
+public class Run {
+    public static void main(String[] args) throws InterruptedException {
+        /**
+         * A B 线程 都会被唤醒
+         */
+        MyService service = new MyService();
+        ThreadA a = new ThreadA(service);
+        ThreadB b = new ThreadB(service);
+        a.setName("A");
+        b.setName("B");
+        a.start();
+        b.start();
+        Thread.sleep(3000);
+        service.signalAll();
+    }
+}
---
+++ part-04/src/io/ilss/must_use_more_condition_error/ThreadB.java
@@ -0,0 +1,23 @@
+package io.ilss.must_use_more_condition_error;
+
+/**
+ * className ThreadB
+ * description
+ *
+ * @author feng
+ * @version 1.0
+ * @date 2019/1/18 下午4:45
+ */
+public class ThreadB extends Thread {
+    private MyService service;
+
+    public ThreadB(MyService service) {
+        this.service = service;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        service.awaitB();
+    }
+}
