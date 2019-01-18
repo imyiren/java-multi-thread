cab77afaf2f6598a058a75b83f7bff96444121da
imyiren <imyiren4@gmail.com>
2019-01-18 19:57:33 +0800

    lock method test 2 and 3

---
+++ part-04/src/io/ilss/lock_method_test_3/test2/Service.java
@@ -0,0 +1,29 @@
+package io.ilss.lock_method_test_3.test2;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * className Service
+ * description
+ *
+ * @author feng
+ * @version 1.0
+ * @date 2019/1/18 下午7:44
+ */
+public class Service {
+    private ReentrantLock lock;
+
+    public Service(boolean isFair) {
+        lock = new ReentrantLock(isFair);
+    }
+
+    public void serviceMethod() {
+        try {
+            System.out.println(lock.isHeldByCurrentThread());
+            lock.lock();
+            System.out.println(lock.isHeldByCurrentThread());
+        } finally {
+            lock.unlock();
+        }
+    }
+}
