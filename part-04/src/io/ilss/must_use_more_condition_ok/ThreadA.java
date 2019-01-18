12566d89e88e96624a38bc870a7c1f584786cf42
imyiren <imyiren4@gmail.com>
2019-01-18 16:59:28 +0800

    must use more condition ok

---
+++ part-04/src/io/ilss/must_use_more_condition_ok/ThreadA.java
@@ -0,0 +1,23 @@
+package io.ilss.must_use_more_condition_ok;
+
+/**
+ * className ThreadA
+ * description
+ *
+ * @author feng
+ * @version 1.0
+ * @date 2019/1/18 下午4:54
+ */
+public class ThreadA extends Thread {
+    private MyService service;
+
+    public ThreadA(MyService service) {
+        this.service = service;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        service.awaitA();
+    }
+}
