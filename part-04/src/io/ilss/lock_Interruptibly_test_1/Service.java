f04d381484f6e40ef18028b1bb11762d483799a4
ilssio <git@ilss.io>
2019-01-18 21:05:04 +0800

    lock interruptibly tset

---
+++ part-04/src/io/ilss/lock_Interruptibly_test_1/Service.java
@@ -0,0 +1,35 @@
+package io.ilss.lock_Interruptibly_test_1;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * className Service
+ * description
+ *
+ * @author feng
+ * @version 1.0
+ * @date 2019/1/18 下午8:47
+ */
+public class Service {
+    public ReentrantLock lock = new ReentrantLock();
+    private Condition condition = lock.newCondition();
+
+    public void waitMethod(){
+        try {
+            lock.lock();
+            System.out
+                    .println("lock begin " + Thread.currentThread().getName());
+            for (int i = 0; i < Integer.MAX_VALUE / 10; i++) {
+                String newString = "";
+                Math.random();
+            }
+            System.out
+                    .println("lock   end " + Thread.currentThread().getName());
+        } finally {
+            if (lock.isHeldByCurrentThread()) {
+                lock.unlock();
+            }
+        }
+    }
+}
