5d1f40d7fecf9da623ffbe9224bb1b50ddb40e25
ilssio <git@ilss.io>
2019-01-18 21:31:12 +0800

    condition 123

---
+++ part-04/src/io/ilss/condition_123/F.java
@@ -0,0 +1,13 @@
+package io.ilss.condition_123;
+
+/**
+ * className F
+ * description
+ *
+ * @author feng
+ * @version 1.0
+ * @date 2019/1/18 下午9:27
+ */
+public class F {
+    volatile public static int nextPrintWho = 1;
+}
