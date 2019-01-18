bbcba40e94b331af1f5a2870c7617a4fc9dcf4fe
imyiren <imyiren4@gmail.com>
2019-01-18 16:23:57 +0800

    z3_ok

---
+++ part-04/src/io/ilss/z3_ok/MyService.java
@@ -0,0 +1,32 @@
+package io.ilss.z3_ok;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * className MyService
+ * description
+ *
+ * @author feng
+ * @version 1.0
+ * @date 2019/1/18 下午3:35
+ */
+public class MyService {
+    private Lock lock = new ReentrantLock();
+    private Condition condition = lock.newCondition();
+
+    public void waitMethod() {
+        try {
+            lock.lock();
+            System.out.println("A");
+            condition.await();
+            System.out.println("B");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            lock.unlock();
+            System.out.println("free lock!");
+        }
+    }
+}
