0afbe871e9ea7e0d25891550e66580f8452b5a90
ilssio <git@ilss.io>
2019-01-18 21:13:36 +0800

    tryLock method

---
+++ part-04/src/io/ilss/try_lock_param/Service.java
@@ -0,0 +1,36 @@
+package io.ilss.try_lock_param;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * className Service
+ * description
+ *
+ * @author feng
+ * @version 1.0
+ * @date 2019/1/18 下午9:08
+ */
+public class Service {
+    public ReentrantLock lock = new ReentrantLock();
+
+    public void waitMethod() {
+        try {
+            //如果3秒没有锁没有被其他线程保持，并且当前线程没有中断就或得锁定
+            if (lock.tryLock(3, TimeUnit.SECONDS)) {
+                System.out.println("      " + Thread.currentThread().getName()
+                        + "获得锁的时间：" + System.currentTimeMillis());
+                Thread.sleep(10000);
+            } else {
+                System.out.println("      " + Thread.currentThread().getName()
+                        + "没有获得锁");
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            if (lock.isHeldByCurrentThread()) {
+                lock.unlock();
+            }
+        }
+    }
+}
