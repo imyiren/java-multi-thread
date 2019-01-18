0feb08cb1ef33579f2aab89ebb3749b5827ed381
ilssio <git@ilss.io>
2019-01-18 14:30:09 +0800

    inheritable thread local

---
+++ part-03/src/io/ilss/part3/inheritable_thread_local/InheritableThreadLocalExt.java
@@ -0,0 +1,17 @@
+package io.ilss.part3.inheritable_thread_local;
+
+/**
+ * className InheritableThreadLocalExt
+ * description
+ *
+ * @author feng
+ * @version 1.0
+ * @date 2019/1/18 下午2:25
+ */
+public class InheritableThreadLocalExt extends InheritableThreadLocal<Long> {
+    @Override
+    protected Long initialValue() {
+        return System.currentTimeMillis();
+    }
+
+}
