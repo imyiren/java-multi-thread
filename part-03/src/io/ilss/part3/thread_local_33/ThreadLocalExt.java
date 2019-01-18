3f8ea7143f5bbf6ce530b32512dac0be07870d00
imyiren <imyiren4@gmail.com>
2019-01-18 14:24:23 +0800

    ThreadLocal33

---
+++ part-03/src/io/ilss/part3/thread_local_33/ThreadLocalExt.java
@@ -0,0 +1,18 @@
+package io.ilss.part3.thread_local_33;
+
+import java.util.Date;
+
+/**
+ * className ThreadLocalExt
+ * description
+ *
+ * @author feng
+ * @version 1.0
+ * @date 2019/1/18 下午2:19
+ */
+public class ThreadLocalExt extends ThreadLocal<Long> {
+    @Override
+    protected Long initialValue() {
+        return System.currentTimeMillis();
+    }
+}
