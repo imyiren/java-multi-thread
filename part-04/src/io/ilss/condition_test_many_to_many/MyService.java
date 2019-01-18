32d950fda371b4aea8f88708f5cffb626de51ea4
ilssio <git@ilss.io>
2019-01-18 17:22:07 +0800

    condition test many to many

--- part-04/src/io/ilss/condition_test_many_to_many/MyService.java
+++ part-04/src/io/ilss/condition_test_many_to_many/MyService.java
@@ -14,18 +14,19 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class MyService {
     private Lock lock = new ReentrantLock();
-    private Condition condition = lock.newCondition();
+    private Condition conditionB = lock.newCondition();
+    private Condition conditionA = lock.newCondition();
     private boolean hasValue = false;
     public void set() {
         try {
             lock.lock();
             while (hasValue) {
                 System.out.println("有可能★★连续");
-                condition.await();
+                conditionA.await();
             }
             System.out.println("打印★");
             hasValue = true;
-            condition.signalAll();
+            conditionB.signalAll();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }finally {
@@ -37,11 +38,11 @@ public class MyService {
             lock.lock();
             while (!hasValue) {
                 System.out.println("有可能☆☆连续");
-                condition.await();
+                conditionB.await();
             }
             System.out.println("打印☆");
             hasValue = false;
-            condition.signalAll();
+            conditionA.signalAll();
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
