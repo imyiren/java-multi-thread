fe06cfd1e84fb302e17c3c7e3de8a1184a939551
ilssio <git@ilss.io>
2019-01-18 15:10:46 +0800

    condition test more method

---
+++ part-04/src/io/ilss/condition_test_more_method/Run.java
@@ -0,0 +1,28 @@
+package io.ilss.condition_test_more_method;
+
+/**
+ * className Run
+ * description
+ *
+ * @author feng
+ * @version 1.0
+ * @date 2019/1/18 下午3:07
+ */
+public class Run {
+    public static void main(String[] args) throws InterruptedException {
+        MyService service = new MyService();
+        ThreadA a = new ThreadA(service);
+        ThreadAA aa = new ThreadAA(service);
+        ThreadB b = new ThreadB(service);
+        ThreadBB bb = new ThreadBB(service);
+        a.setName("A");
+        aa.setName("AA");
+        b.setName("B");
+        bb.setName("BB");
+        a.start();
+        aa.start();
+        Thread.sleep(100);
+        b.start();
+        bb.start();
+    }
+}
