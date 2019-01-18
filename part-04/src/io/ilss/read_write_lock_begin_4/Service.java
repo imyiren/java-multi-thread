22893ba1c28f01a1bd875f1246254845b878beca
ilssio <git@ilss.io>
2019-01-18 21:44:39 +0800

    ReentrantReadWriteLock

---
+++ part-04/src/io/ilss/read_write_lock_begin_4/Service.java
@@ -0,0 +1,48 @@
+package io.ilss.read_write_lock_begin_4;
+
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * className Service
+ * description
+ *
+ * @author feng
+ * @version 1.0
+ * @date 2019/1/18 下午9:35
+ */
+public class Service {
+
+    private ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+
+    public void read() {
+        try {
+            try {
+                lock.readLock().lock();
+                System.out.println("获得读锁" + Thread.currentThread().getName()
+                        + " " + System.currentTimeMillis());
+                Thread.sleep(10000);
+            } finally {
+                lock.readLock().unlock();
+            }
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public void write() {
+        try {
+            try {
+                lock.writeLock().lock();
+                System.out.println("获得写锁" + Thread.currentThread().getName()
+                        + " " + System.currentTimeMillis());
+                Thread.sleep(10000);
+            } finally {
+                lock.writeLock().unlock();
+            }
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
