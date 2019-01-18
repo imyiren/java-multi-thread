83c108ba80ab4102ff824f4cf63b3b5c06ddd1fa
imyiren <imyiren4@gmail.com>
2019-01-18 21:46:53 +0800

    part05

---
+++ part-05/src/io/ilss/part5/timer_test_1/Run1.java
@@ -0,0 +1,12 @@
+package io.ilss.part5.timer_test_1;
+
+/**
+ * className Run1
+ * description
+ *
+ * @author feng
+ * @version 1.0
+ * @date 2019/1/18 下午9:46
+ */
+public class Run1 {
+}
---
+++ part-05/src/io/ilss/part5/timer_test_2/Run.java
@@ -0,0 +1,12 @@
+package io.ilss.part5.timer_test_2;
+
+/**
+ * className Run
+ * description
+ *
+ * @author feng
+ * @version 1.0
+ * @date 2019/1/18 下午9:46
+ */
+public class Run {
+}
