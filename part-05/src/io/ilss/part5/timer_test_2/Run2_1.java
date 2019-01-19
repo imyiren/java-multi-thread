bffbcb91aa78edc860c749c12eac05a1fdabff4f
imyiren <imyiren4@gmail.com>
2019-01-19 23:37:55 +0800

    timer test 2

---
+++ part-05/src/io/ilss/part5/timer_test_2/Run2_1.java
@@ -0,0 +1,40 @@
+package io.ilss.part5.timer_test_2;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * className Run2_1
+ * description Run2_1
+ *
+ * @author feng
+ * @version 1.0
+ * @date 2019-01-19 23:34
+ */
+public class Run2_1 {
+    public static void main(String[] args) throws ParseException {
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("Begin，time : " + new Date());
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("  End，time : " + new Date());
+            }
+        };
+        Timer timer = new Timer();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        //在这个时间运行上面的定时器任务
+        String dateString = "2019-01-19 22:13:00";
+        Date date = sdf.parse(dateString);
+        System.out.println("dateString : " + date.toLocaleString() + " CurrentDate : " + new Date().toLocaleString());
+        //由于任务执行时间过长，间隔时间被延迟。
+        timer.schedule(task, date,4000);
+    }
+}
