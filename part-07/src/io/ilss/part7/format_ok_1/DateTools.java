522af34001017f2cb9db6d70f7b36b6a5088feac
imyiren <imyiren4@gmail.com>
2019-01-20 18:05:22 +0800

    format date string

---
+++ part-07/src/io/ilss/part7/format_ok_1/DateTools.java
@@ -0,0 +1,24 @@
+package io.ilss.part7.format_ok_1;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * className DateTools
+ * description DateTools
+ *
+ * @author feng
+ * @version 1.0
+ * @date 2019-01-20 18:00
+ */
+public class DateTools {
+    public static Date parse(String formatPattern, String dateString)
+            throws ParseException {
+        return new SimpleDateFormat(formatPattern).parse(dateString);
+    }
+
+    public static String format(String formatPattern, Date date) {
+        return new SimpleDateFormat(formatPattern).format(date).toString();
+    }
+}
